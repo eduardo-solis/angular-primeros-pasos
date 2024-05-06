ca46bac92ded1c25d87b8d9b8cb7f0599026e334
Eduardo Solis <solishernandezeduardoruben@gmail.com>
2024-05-06 16:02:25 -0600

    Emitiendo(output) desde el componente list

--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Character } from '../../interfaces/character.interface';
 
 @Component({
@@ -7,12 +7,21 @@ import { Character } from '../../interfaces/character.interface';
   styleUrl: './list.component.css'
 })
 export class ListComponent {
-  
+
   @Input()
   public characterList: Character[] = [
     {
       name: 'Trunks',
       power: 10
     }]
+
+  @Output()
+  public onDelete: EventEmitter<number> = new EventEmitter();
+
+  onDeleteCharacter(index: number): void {
+    // TODO: Emitir el ID del personaje
+    console.log({index});
+    this.onDelete.emit(index);
+  }
 
 }
