a92127e8b5897a1dd80b32d53064da972dc90a9d
Eduardo Solis <solishernandezeduardoruben@gmail.com>
2024-05-05 22:17:33 -0600

    Modulo counter

---
+++ src/app/heroes/hero/hero.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-hero',
+  templateUrl: './hero.component.html',
+  styleUrl: './hero.component.css'
+})
+export class HeroComponent {
+
+  public name:  string = 'ironMan';
+  public age:   number = 45;
+
+  get capitalizeName(): string {
+    return this.name.toUpperCase();
+  }
+
+  getHeroDescription(): string {
+    return `${this.name} - ${this.age}`;
+  }
+
+  changeHero(): void {
+    this.name = 'Spiderman';
+  }
+
+  changeAge(): void {
+    this.age = 25;
+  }
+
+  resetForm(): void {
+    this.name = 'ironMan';
+    this.age = 45;
+
+    //document.querySelectorAll('h1')?.forEach((element) => {
+    //  element.innerHTML="<h1>Desde Angular</h1>";
+    //})
+  }
+
+}
